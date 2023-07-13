276710a829ec785bb6b86ed15422ce30253217bd
popoxta <popoxta@gmail.com>
2023-07-14 09:13:30 +1200

    Add state

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,4 +1,4 @@
-import React, {ReactElement} from "react";
+import React, {ReactElement, useState} from "react";
 import Buttons from "./Buttons.tsx";
 import OperandButtons from "./OperandButtons.tsx";
 
@@ -6,14 +6,16 @@ import OperandButtons from "./OperandButtons.tsx";
 // yes this is my first time using ts, have mercy
 
 export default function Calculator() {
+    const [equation, setEquation] = useState([])
 
     function handleClick(e): void {
         console.log(e.target.id)
+        setEquation(prev => [...prev, e.target.id])
     }
 
     return (
         <main id={'calculator'}>
-            <h2 id={'calculator-screen'}></h2>
+            <h2 id={'calculator-screen'}>{equation}</h2>
             <Buttons handleClick={handleClick}/>
             <OperandButtons handleClick={handleClick}/>
         </main>

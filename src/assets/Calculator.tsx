67d0d8778190d19e7123bbf4dd483835df31b430
popoxta <popoxta@gmail.com>
2023-07-14 08:23:13 +1200

    Render buttons

--- src/assets/Calculator.tsx
+++ src/assets/Calculator.tsx
@@ -1,4 +1,22 @@
+import {ReactElement} from "react";
+
+
+// yes this is my first time using ts, have mercy
 
 export default function Calculator(){
-    
+
+    const buttons = (): ReactElement[] => {
+        return new Array(10).fill(0).map((el : number, i : number): ReactElement => {
+            return <button key={`btn-${i}`} id={`btn-${i}`}>{i}</button>
+        })
+    }
+
+    return (
+        <main id={'calculator'}>
+            <h2 id={'calculator-screen'}></h2>
+            <div className={'calculator-buttons'}>
+                {buttons()}
+            </div>
+        </main>
+    )
 }

00f1b1de8d95aefd7c813135a91790fcd78318b7
popoxta <popoxta@gmail.com>
2023-07-14 08:31:06 +1200

    Add click handler

--- src/assets/Calculator.tsx
+++ src/assets/Calculator.tsx
@@ -1,13 +1,20 @@
-import {ReactElement} from "react";
+import React, {ReactElement} from "react";
 
 
 // yes this is my first time using ts, have mercy
 
-export default function Calculator(){
+export default function Calculator() {
+
+    function handleClick(e):void {
+        console.log(e.target.id)
+    }
 
     const buttons = (): ReactElement[] => {
-        return new Array(10).fill(0).map((el : number, i : number): ReactElement => {
-            return <button key={`btn-${i}`} className={'calculator-btn'} id={`btn-${i}`}>{i}</button>
+        return new Array(10).fill(0).map((el: number, i: number): ReactElement => {
+            return <button key={`btn-${i}`} className={'calculator-btn'} id={i.toString()}
+                onClick={handleClick}>
+                {i}
+            </button>
         })
     }
 

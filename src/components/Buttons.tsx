b53501bfd2cc48373aea0a8a0ad12d5b0d0f3ec5
popoxta <popoxta@gmail.com>
2023-07-14 08:40:06 +1200

    Pull out buttons

---
+++ src/components/Buttons.tsx
@@ -0,0 +1,24 @@
+import React, {ReactElement} from "react";
+
+export default function Buttons({handleClick}) {
+
+    const buttons = new Array(10).fill(0).map(
+        (el: number, i: number): ReactElement => {
+            return (
+                <button
+                    key={`btn-${i}`}
+                    className={'calculator-btn'}
+                    id={i.toString()}
+                    onClick={handleClick}>
+                    {i}
+                </button>
+            )
+        })
+
+
+    return (
+        <div className={'calculator-buttons'}>
+            {buttons}
+        </div>
+    )
+}

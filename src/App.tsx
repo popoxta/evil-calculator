5842cc75efb533f63249a18afc34b0fe11bdc352
popoxta <popoxta@gmail.com>
2023-07-14 08:07:24 +1200

    Render calculator component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
+import Calculator from "./assets/Calculator.tsx";
+
 function App() {
 
-  return <h1>Test</h1>
+    return (
+        <>
+            <h1>Test</h1>
+            <Calculator/>
+        </>
+    )
 }
 
 export default App

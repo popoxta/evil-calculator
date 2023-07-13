1b4644152dc2e038e09e801d855f0db3935844d7
popoxta <popoxta@gmail.com>
2023-07-14 08:43:27 +1200

    Add operand buttons

---
+++ src/components/OperandButtons.tsx
@@ -0,0 +1,24 @@
+import {ReactElement} from "react";
+
+export default function OperandButtons({handleClick}) {
+
+    const operands: string[] = ['+', '-', '*', '/']
+    const operandButtons = operands.map(
+        (op: string): ReactElement => {
+            return (
+                <button
+                    key={`btn-${op}`}
+                    className={'operand-btn'}
+                    id={op}
+                    onClick={handleClick}>
+                </button>
+            )
+        }
+    )
+
+    return (
+        <div className={'calculator-operands'}>
+            {operandButtons}
+        </div>
+    )
+}

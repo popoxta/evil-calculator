012df217b5489b56ecfc728faaf3b332497488a6
popoxta <popoxta@gmail.com>
2023-07-14 08:26:01 +1200

    Add grid button display

--- src/assets/Calculator.tsx
+++ src/assets/Calculator.tsx
@@ -7,7 +7,7 @@ export default function Calculator(){
 
     const buttons = (): ReactElement[] => {
         return new Array(10).fill(0).map((el : number, i : number): ReactElement => {
-            return <button key={`btn-${i}`} id={`btn-${i}`}>{i}</button>
+            return <button key={`btn-${i}`} className={'calculator-btn'} id={`btn-${i}`}>{i}</button>
         })
     }
 

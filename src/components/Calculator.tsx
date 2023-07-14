0b87ad13b4db9df824f867fb5db90a0440ba498c
popoxta <popoxta@gmail.com>
2023-07-14 13:32:23 +1200

    Add types

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -8,13 +8,13 @@ import OperandButtons from "./OperandButtons.tsx";
 export default function Calculator() {
     const [equation, setEquation] = useState([])
 
-    const operands = new RegExp(/[/*+]/)
-    const allOperands = new RegExp(/[*+/-]/)
+    const operands: RegExp = new RegExp(/[/*+]/)
+    const allOperands: RegExp = new RegExp(/[*+/-]/)
 
     function handleClick(e): void {
         const current: string = e.target.id
-        const prev = equation[equation.length - 1] ?? ''
-        const beforePrev = equation[equation.length - 2] ?? ''
+        const prev: string = equation[equation.length - 1] ?? ''
+        const beforePrev: string = equation[equation.length - 2] ?? ''
 
         // early exit checks
         if (
@@ -23,12 +23,12 @@ export default function Calculator() {
             || prev[prev.length - 1] === '.' && isNaN(+current)
             || prev.match(allOperands) && current === '.'
         ) {
-            return;
+            return
         }
 
         if (!isNaN(+current) || current === '.') {
             if (equation.length > 0 && !isNaN(+prev)) {
-                const currEquation = [...equation]
+                const currEquation: string[] = [...equation]
                 currEquation[currEquation.length - 1] += current
                 setEquation(currEquation)
                 return
@@ -36,8 +36,8 @@ export default function Calculator() {
         }
 
         // check for double operand and replace prev
-        if (!isNaN(beforePrev) && prev.match(allOperands) && current.match(operands)) {
-            const currEquation = [...equation]
+        if (!isNaN(+beforePrev) && prev.match(allOperands) && current.match(operands)) {
+            const currEquation: string[] = [...equation]
             currEquation[currEquation.length - 1] = current
             setEquation(currEquation)
             return
@@ -45,7 +45,7 @@ export default function Calculator() {
 
         // handles double minus logic
         if ((current.match(allOperands) && prev === '-') && (beforePrev.match(allOperands) || beforePrev === '-')) {
-            const currEquation = [...equation]
+            const currEquation: string[] = [...equation]
             currEquation.pop()
             currEquation[currEquation.length - 1] = current
             setEquation(currEquation)

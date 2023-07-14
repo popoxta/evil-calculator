88d4ae242edb935586be007fda54143f91a76774
popoxta <popoxta@gmail.com>
2023-07-14 13:17:44 +1200

    Add . operand

--- src/components/OperandButtons.tsx
+++ src/components/OperandButtons.tsx
@@ -2,7 +2,7 @@ import {ReactElement} from "react";
 
 export default function OperandButtons({handleClick}) {
 
-    const operands: string[] = ['+', '-', '*', '/']
+    const operands: string[] = ['+', '-', '*', '/', '.']
     const operandButtons = operands.map(
         (op: string): ReactElement => {
             return (
